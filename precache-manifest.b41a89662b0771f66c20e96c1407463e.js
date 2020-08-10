5479feedab1a64b0708f572df71d7b8783d26c26
Haocheng <yuanhaocheng910505@gmail.com>
2020-08-10 17:35:13 +1000

    Updates

---
+++ precache-manifest.b41a89662b0771f66c20e96c1407463e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e041d376e10533918c979c3c1889fae0",
+    "url": "/react-basketball-scoreboard/index.html"
+  },
+  {
+    "revision": "24133a5d9491aa036266",
+    "url": "/react-basketball-scoreboard/static/css/main.422b932a.chunk.css"
+  },
+  {
+    "revision": "9f6667b29e70656bf1c3",
+    "url": "/react-basketball-scoreboard/static/js/2.56362c60.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-basketball-scoreboard/static/js/2.56362c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24133a5d9491aa036266",
+    "url": "/react-basketball-scoreboard/static/js/main.5f6efc9b.chunk.js"
+  },
+  {
+    "revision": "873fcbab67236d2f6469",
+    "url": "/react-basketball-scoreboard/static/js/runtime-main.d316bacf.js"
+  }
+]);
